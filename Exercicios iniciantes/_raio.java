f6231d68cd2a7a827f39a70502fe53985f61d09c
Beatriz Salles <beatriz-lua123@hotmail.com>
2023-10-02 18:29:48 -0300

    mais algumas formulas

---
+++ Exercicios iniciantes/_raio.java
@@ -0,0 +1,34 @@
+  import java.util.Locale;
+import java.util.Scanner;
+  
+  public class _raio {
+    public static void main(String[] args){
+         /*Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
+    casas decimais conforme exemplos.
+    Fórmula da área: area = π . raio2 (ao quadrado)
+    Considere o valor de π = 3.14159 */
+
+    Scanner sc = new Scanner(System.in);
+
+    double x, area, raio;
+    Locale.setDefault(Locale.US);
+
+    x = sc.nextDouble();
+    raio = Math.pow(x, 2);
+    area = (3.14159 * raio);
+
+   //System.out.println("gmnkoegnier " + area);
+    System.out.printf("Area = %.4f%n", area);
+
+    
+
+    sc.close();
+
+    }
+  
+   
+
+
+
+    
+}
--- Exercicios iniciantes/_soma.java
+++ Exercicios iniciantes/_soma.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
 public class _soma{
+    /*Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
+     mensagem explicativa */
     public static void main(String[] args) {
      Scanner sc = new Scanner(System.in);
 
