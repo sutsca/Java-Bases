df9a06c9951b53824947e01596d4208e24f3f462
Beatriz Salles <beatriz-lua123@hotmail.com>
2023-10-04 19:23:19 -0300

    Create _menu.java

---
+++ Exercicios estrutura condicional/_menu.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+  
+  public class _menu {
+    public static void main(String[] args) {
+
+        /* 
+        
+        Com base na tabela abaixo, escreva um programa que leia o código de um item e a quantidade deste item. A
+        seguir, calcule e mostre o valor da conta a pagar.
+         
+        */
+
+        Scanner sc = new Scanner(System.in);
+
+        int pedido,qtd;
+
+        double total = 0.0;
+
+        System.out.print("Qual o número do pedido gostaria? ");
+        pedido = sc.nextInt();
+        System.out.print("Qual a quantidade? ");
+        qtd = sc.nextInt();
+
+        if (pedido == 1){
+
+            total = (qtd * 4.00);
+
+           } else if(pedido == 2) {
+
+              total = (qtd * 4.50);
+
+              }
+                if (pedido == 3){
+
+                total = (qtd * 5.00);
+
+                } else if(pedido == 4) {
+
+                   total = (qtd * 2.00);
+
+                  }
+                    if (pedido == 5){
+
+                     total = (qtd * 1.50);
+                     } else{
+                      System.out.println("Pedido inválido.");
+                      }
+                        if (total > 0.0) {
+                         System.out.printf("Total: R$ %.2f%n", total);
+                        }
+
+                        sc.close();
+    }
+}
